212f486ae3e8250cac2981cee70d90c59ed5128b
Matt Amos <zerebubuth@gmail.com>
2015-08-22 16:06:55 +0100

    Added missing copyright headers.

--- test/test_request_zxy.c
+++ test/test_request_zxy.c
@@ -1,3 +1,10 @@
+/* This file is part of Vecamole.
+ * Copyright (c) Matt Amos <zerebubuth@gmail.com>. All rights reserved.
+ * Redistribution allowed under the terms of the 2-clause BSD license
+ * distributed with the source code, or available at
+ * http://github.com/zerebubuth/vecamole/blob/master/COPYING
+ */
+
 #include "vecamole.h"
 #include <stdlib.h>
 #include <stdio.h>
