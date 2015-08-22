5d6073bb417d2c6a5f7eaf4e82a91dc8c91e4014
Matt Amos <zerebubuth@gmail.com>
2015-08-22 16:03:53 +0100

    Trying to make sure the package builds from a clean checkout, and gives appropriate errors when stuff is missing.

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+function check {
+    file="deps/$1/$2"
+    if [ ! -f "$file" ]; then
+	echo "Dependency on $1 appears to be missing. You might need to run 'git submodule update --init --recursive."
+	exit 1
+    fi
+}
+
+check "mapnik-vector-tile" "src/vector_tile_backend_pbf.hpp"
+check "mapnik-vector-tile" "src/vector_tile_processor.hpp"
+check "clipper" "cpp/clipper.hpp"
+check "protozero" "include/protozero/pbf_reader.hpp"
+check "protozero" "include/protozero/pbf_writer.hpp"
+
 libtoolize
 aclocal -I m4
 autoreconf --force --install
